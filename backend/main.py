f053c66881e4c07585c4e9119916e62e39f014d5
ysumayyah284-star <ysumayyah284@gmail.com>
2025-12-27 19:36:06 -0500

    API Foundations

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,16 @@ app = FastAPI(title="FAA")
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/")
+def endpoint():
+    return {"name": "FAA","status": "running"}
+
+transactions = {"date": "08/31", "amount": 1000, "name": "rent"}
+@app.get("/transaction")
+def transaction():
+    return transactions
+
+@app.get("/version")
+def version():
+    return {"version": "0.1.0"}
